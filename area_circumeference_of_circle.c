973e6722631cf4e5f068bb186960b19b3da40b14
Venancio Aaron Fernandes <fernandesvenancio22@gmail.com>
2026-08-23 11:52:22 +0530

    Add files via upload

---
+++ area_circumeference_of_circle.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    float r = 5.5, area, circumference;
+    area = 3.14*r*r;
+    circumference = 2 * 3.14*r;
+    printf("area is %f\n", area);
+    printf("circumference is %f\n", circumference);
+    return 0;
+}
